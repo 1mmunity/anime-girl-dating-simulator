e42ca30e8af927abbb0e38cc42785a9b4fa83302
1mmunity <arvinhijinks@gmail.com>
2022-03-29 22:53:36 +0800

    g

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ function MyApp({ Component, pageProps }) {
     <title>Anime Girl Dating Simulator</title>
     <meta name="title" content="Anime Girl Dating Simulator" />
     <meta name="description" content="A game where you can date anime characters." />
+    <link rel="icon" href="/icon.jpg" />
 
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://anime-girl-dating-simulator.vercel.app" />
